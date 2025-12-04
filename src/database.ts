8cbf20282cb59488c9e06521cb3c0d6271399089
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-12-04 19:02:35 +0100

    +

--- src/database.ts
+++ src/database.ts
@@ -6,14 +6,13 @@ dotenv.config();
 const POSTGRES_HOST = process.env.POSTGRES_HOST;
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 const POSTGRES_USER = process.env.POSTGRES_USER;
-const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
 
-console.log(`POSTGRES_HOST:${POSTGRES_HOST} POSTGRES_USER:${POSTGRES_USER} POSTGRES_DATABASE:${POSTGRES_DATABASE}`)
+console.log(`POSTGRES_HOST:${POSTGRES_HOST} POSTGRES_USER:${POSTGRES_USER} `)
 
 const pool = new Pool({
   host: POSTGRES_HOST || 'localhost',
   user: POSTGRES_USER || 'postgres',
-  database: POSTGRES_DATABASE || "bookmarkdb",
+  database: "bookmarkdb",
   password: POSTGRES_PASSWORD,
   port:5432,
   max: 20,
@@ -22,18 +21,20 @@ const pool = new Pool({
   maxLifetimeSeconds: 60
 })
 
-// const client = await pool.connect()
-// const res = await client.query('SELECT id, created_at, "text", user_agent, ip_address, device_hash FROM public.bookmarks;')
-// console.log(res.rows[0])
-// client.release()
 
 
-async function createDatabase(name:string){
-
+export async function get(name:string){
     const client = await pool.connect();
-    const res = await client.query(`CREATE DATABASE ${name};`);
-    console.log(res.rows[0]);
+    const res = await client.query(`SELECT * FROM ${name};`);
+    // console.log(res.rows[0]);
+    client.release();
+    return res
+}
+
+export async function add(text:string){
+    const client = await pool.connect();
+    await client.query(`INSERT INTO bookmarks ("text", user_agent, ip_address, device_hash) VALUES('${text}', 'user_agent', ip, 'hash');`);
     client.release();
 }
 
-export default createDatabase;
+// export default get;add;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,88 @@
-import app from "./server.ts"
-import createDatabase from "./database.ts";
+import {get,add} from "./database.ts";
+import dotenv from "dotenv";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import Fastify from 'fastify'
+
+dotenv.config();
 
 const port = 3000;
+const app = Fastify({
+  logger: true,
+})
+
+const swaggerOptions = {
+    swagger: {
+        info: {
+            title: "BookmarksDB",
+            description: "My Description.",
+            version: "1.0.0",
+        },
+        host: "localhost",
+        schemes: ["http", "https"],
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        tags: [{ name: "Default", description: "Default" }],
+    },
+};
+
+const swaggerUiOptions = {
+    routePrefix: "/docs",
+    exposeRoute: true,
+};
+
+app.register(fastifySwagger, swaggerOptions);
+app.register(fastifySwaggerUi, swaggerUiOptions);
+
+app.register((app, options, done) => {
+    app.get("/", {
+        schema: {
+            tags: ["Default"],
+            response: {
+                200: {
+                    type: "object",
+                    properties: {
+                        anything: { type: "string" },
+                    },
+                },
+            },
+        },
+        handler: (req, res) => {
+            res.send({ anything: "meaningfull" });
+        },
+    });
 
 
-app.get("/create-database",(req,res) => {
-    createDatabase("secrets")
+
+    app.get('/hello', (request, reply) => {
+      reply.send({ hello: 'world' })
+    })
+
+    app.get('/get', async (request, reply) => {
+        const res = await get();
+        reply.send(res)
+    })
+
+    app.get('/add/:text', async (request, reply) => {
+        await add(request.params.text)
+    })
+
+
+    done();
 });
 
-app.listen(3000, () => {
-    console.log("hi");
-});
+app.listen(
+    {
+        port: Number(process.env.APP_PORT) ?? 3000, // Pulled from env file.
+        host: '',
+    },
+    (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+);
 
+
+export default app;
